96277c8d7018b94a87eb2b9565fa083c9cfb46b4
Gülers <jesus3419@gmail.com>
2022-10-16 11:59:58 +0300

    First Commit

---
+++ src/main/java/com/isaguler/socketioserverdemo/SocketioServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.isaguler.socketioserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketioServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketioServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaguler/socketioserverdemo/SocketioServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaguler.socketioserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketioServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
